cb069597a610bcb9e62b460f85c533a24e23c821
Kriegsmarine13 <rediska195@mail.ru>
2025-08-06 14:29:47 +0300

    change logger assignment of any to request type

--- src/common/logger.interceptor.ts
+++ src/common/logger.interceptor.ts
@@ -5,26 +5,29 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
+import { Request } from 'express';
 
 @Injectable()
 export class LoggerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<Request>();
     const { method, url, query } = request;
     const start = Date.now();
 
     return next.handle().pipe(
       tap((data) => {
         const duration = Date.now() - start;
-        console.log(JSON.stringify({
-          level: 'info',
-          time: new Date().toJSON(),
-          method,
-          url,
-          query,
-          duration_ms: duration,
-          status: data?.error?.code || 200,
-        }));
+        console.log(
+          JSON.stringify({
+            level: 'info',
+            time: new Date().toJSON(),
+            method,
+            url,
+            query,
+            duration_ms: duration,
+            status: data?.error?.code || 200,
+          }),
+        );
       }),
     );
   }
